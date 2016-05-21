2cea96c5d44167b0a95a5020bbb057a8ea39951c
nikolalsvk <nikolaseap@gmail.com>
2016-05-21 17:17:55 +0200

    Add Sign In and Sign Out

---
+++ client/src/app/components/userAuth/sign_in.controller.js
@@ -0,0 +1,5 @@
+export class SignInController {
+  constructor ($scope) {
+    'ngInject';
+  }
+}
--- client/src/app/index.route.js
+++ client/src/app/index.route.js
@@ -6,6 +6,12 @@ export function routerConfig ($stateProvider, $urlRouterProvider) {
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
       controllerAs: 'main'
+    })
+    .state('signIn', {
+      url: '/sign_in',
+      templateUrl: 'app/components/userAuth/new.html',
+      controller: 'SignInController',
+      controllerAs: 'sign_in'
     });
 
   $urlRouterProvider.otherwise('/');

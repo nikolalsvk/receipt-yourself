b6678f0f9f3684a46e087668e885cf2e6908b84c
besermenji <besermenji.vladimir@gmail.com>
2016-05-30 23:09:13 +0200

    Company spec updated and factory added

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
+  before do
+    FactoryGirl.create(:company)
+  end
+
   it { should validate_uniqueness_of(:name) }
   it { should validate_presence_of(:name) }
 end

088a1b5ce4b7a402a9c9a4bc636eb8f265c2eea6
besermenji <besermenji.vladimir@gmail.com>
2016-05-31 21:36:49 +0200

    Bank model and test updated

--- db/migrate/20160530170100_create_banks.rb
+++ db/migrate/20160530170100_create_banks.rb
@@ -1,7 +1,7 @@
 class CreateBanks < ActiveRecord::Migration
   def change
     create_table :banks do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps null: false
     end

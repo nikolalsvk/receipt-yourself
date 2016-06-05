177d5e8225f0b88e2f894b52aa5d6cf8718137d4
besermenji <besermenji.vladimir@gmail.com>
2016-06-05 10:56:06 +0200

    Updated Gemfile with schema information gem

--- app/models/daily_bank_statement.rb
+++ app/models/daily_bank_statement.rb
@@ -1,6 +1,6 @@
 class DailyBankStatement < ActiveRecord::Base
   validates :number, presence: true,
-  									 uniqueness: true,
-  									 case_sensitive: false
+                     uniqueness: true,
+                     case_sensitive: false
   validates :statement_date, presence: true
 end

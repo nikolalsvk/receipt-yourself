05a1bcc627805287bd2cbc47c666106304b2f1ed
besermenji <besermenji.vladimir@gmail.com>
2016-06-07 16:01:39 +0200

    Payment proposal model and specs updated

--- spec/factories/payment_proposal_factory.rb
+++ spec/factories/payment_proposal_factory.rb
@@ -1,12 +1,19 @@
+
 # == Schema Information
 #
 # Table name: payment_proposals
 #
 #  id           :integer          not null, primary key
-#  payment_date :datetime
-#  number       :integer
-#  status       :integer
+#  payment_date :datetime         not null
+#  number       :string           not null
+#  status       :integer          not null
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
-
+FactoryGirl.define do
+  factory :payment_proposal do
+    payment_date { Date.today }
+    number { Faker::Number.number(6) }
+    status { 1 }
+  end
+end
--- spec/models/payment_proposal_spec.rb
+++ spec/models/payment_proposal_spec.rb
@@ -3,9 +3,9 @@
 # Table name: payment_proposals
 #
 #  id           :integer          not null, primary key
-#  payment_date :datetime
-#  number       :integer
-#  status       :integer
+#  payment_date :datetime         not null
+#  number       :string           not null
+#  status       :integer          not null
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
@@ -13,5 +13,12 @@
 require 'rails_helper'
 
 RSpec.describe PaymentProposal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    FactoryGirl.create(:payment_proposal)
+  end
+
+  it { should validate_presence_of(:payment_date) }
+  it { should validate_presence_of(:number) }
+  it { should validate_uniqueness_of(:number).case_insensitive }
+  it { should validate_presence_of(:status) }
 end

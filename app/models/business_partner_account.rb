769a9d7a9a0a25276769111c94abc0b6076d94d8
besermenji <besermenji.vladimir@gmail.com>
2016-06-10 00:21:41 +0200

    Assciation between bank and business partner accounts

--- app/models/business_partner_account.rb
+++ app/models/business_partner_account.rb
@@ -5,11 +5,14 @@
 #  id         :integer          not null, primary key
 #  number     :string           not null
 #  activated  :boolean          default(FALSE)
+#  bank_id    :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 
 class BusinessPartnerAccount < ActiveRecord::Base
+  belongs_to :bank
+
   validates :number, presence: true,
   					 uniqueness: true,
   					 case_sensitive: false
--- spec/models/business_partner_account_spec.rb
+++ spec/models/business_partner_account_spec.rb
@@ -5,6 +5,7 @@
 #  id         :integer          not null, primary key
 #  number     :string           not null
 #  activated  :boolean          default(FALSE)
+#  bank_id    :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #

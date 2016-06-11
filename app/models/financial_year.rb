81961165f2d2d9aee802a038b92351d6d451fed9
besermenji <besermenji.vladimir@gmail.com>
2016-06-11 17:05:03 +0200

    Associations for business year

--- app/models/financial_year.rb
+++ app/models/financial_year.rb
@@ -12,6 +12,8 @@
 
 class FinancialYear < ActiveRecord::Base
   belongs_to :company
+  has_many :input_invoices
+  has_many :output_invoices
 
   validates :year, presence: true,
                    length: { minimum: 4,

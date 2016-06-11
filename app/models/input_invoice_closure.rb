03364b711e8c402cefaf836324f9468a689dd30f
besermenji <besermenji.vladimir@gmail.com>
2016-06-11 16:55:13 +0200

    Associations for daily statement

--- app/models/input_invoice_closure.rb
+++ app/models/input_invoice_closure.rb
@@ -2,14 +2,17 @@
 #
 # Table name: input_invoice_closures
 #
-#  id             :integer          not null, primary key
-#  closure_date   :datetime         not null
-#  closure_amount :decimal(15, 4)   not null
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
+#  id                 :integer          not null, primary key
+#  closure_date       :datetime         not null
+#  closure_amount     :decimal(15, 4)   not null
+#  daily_statement_id :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
 #
 
 class InputInvoiceClosure < ActiveRecord::Base
+  belongs_to :daily_statement  
+
   validates :closure_date, presence: true
   validates :closure_amount, presence: true
 end
--- spec/factories/output_invoice_closure_factory.rb
+++ spec/factories/output_invoice_closure_factory.rb
@@ -2,11 +2,12 @@
 #
 # Table name: output_invoice_closures
 #
-#  id             :integer          not null, primary key
-#  closure_date   :datetime         not null
-#  closure_amount :decimal(15, 4)   not null
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
+#  id                 :integer          not null, primary key
+#  closure_date       :datetime         not null
+#  closure_amount     :decimal(15, 4)   not null
+#  daily_statement_id :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
 #
 
 FactoryGirl.define do

d471dced9db2eb2aa3f2e734b07410102548813f
besermenji <besermenji.vladimir@gmail.com>
2016-06-11 17:24:52 +0200

    Payment forming associations

--- app/models/payment_forming.rb
+++ app/models/payment_forming.rb
@@ -2,17 +2,19 @@
 #
 # Table name: payment_formings
 #
-#  id                 :integer          not null, primary key
-#  amount             :decimal(15, 4)   not null
-#  company_account_id :integer
-#  input_invoice_id   :integer
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
+#  id                  :integer          not null, primary key
+#  amount              :decimal(15, 4)   not null
+#  company_account_id  :integer
+#  input_invoice_id    :integer
+#  payment_proposal_id :integer
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
 #
 
 class PaymentForming < ActiveRecord::Base
   belongs_to :company_account
   belongs_to :input_invoice
+  belongs_to :payment_proposal
 
   validates :amount, presence: true
 end

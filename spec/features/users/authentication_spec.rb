db17d8e894d0454a41616c84e4bea666b97e39ef
aleksandarbos <aleksandarbos.ns@gmail.com>
2016-06-21 17:25:18 +0200

    Implemented frontpage login form, and written rspec scenario.

--- spec/features/users/authentication_spec.rb
+++ spec/features/users/authentication_spec.rb
@@ -4,20 +4,38 @@ feature 'Authentication', js: true do
   before do
     @user = FactoryGirl.create(:confirmed_user)
 
-    visit '/sign_in'
+    visit '/'  
   end
 
   feature 'login' do
+    before do
+      visit '/sign_in'
+    end
+    scenario 'redirect after login' do
+      login(@user.email, @user.password)
+
+      expect(page).to have_content('You are signed in as')
+    end
     scenario 'with invalid credentials' do
       login("lol@lol.com", "testtest")
 
       expect(page).to have_content('Invalid credentials')
     end
+  end
 
+  feature 'frontpage-login' do
+    before do
+      visit '/'
+    end
     scenario 'redirect after login' do
       login(@user.email, @user.password)
 
       expect(page).to have_content('You are signed in as')
+    end
+    scenario 'with invalid credentials' do
+      login("lol@lol.com", "testtest")
+
+      expect(page).to have_content('Invalid credentials')
     end
   end
 
@@ -26,4 +44,5 @@ feature 'Authentication', js: true do
     fill_in "Password", with: password
     find("button", text: "SIGN IN").click
   end
+
 end

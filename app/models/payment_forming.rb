910f68ac03f721ac7d6c59c8dca8e2179501a095
besermenji <besermenji.vladimir@gmail.com>
2016-06-28 23:00:28 +0200

    Callbacks for updating payment formings and payment order (related to amounts)

--- app/models/payment_forming.rb
+++ app/models/payment_forming.rb
@@ -12,9 +12,25 @@
 #
 
 class PaymentForming < ActiveRecord::Base
+  before_validation :set_amount, on: :create
+
   belongs_to :company_account
   belongs_to :input_invoice
   belongs_to :payment_proposal
 
+  # I guess that one payment_forming
+  #   can have one payment_order?
+  has_one :payment_order
+
   validates :amount, presence: true
+
+  private
+
+  # when looking at the model
+  #   it seems to me that this amout is the
+  #   same amount as payment_amount in 
+  #   input invoice, hope that this is okay
+  def set_amount
+    self.amount = input_invoice.payment_amount
+  end
 end

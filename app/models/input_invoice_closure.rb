96eeed495cbd9f8fe5f6716c5b2a18adb5f9bf2c
besermenji <besermenji.vladimir@gmail.com>
2016-06-29 07:14:36 +0200

    If amounts dont match you cannot create closure, also for creating input invoice closure you will need only ids from daily_statement and input_invoice

--- app/models/input_invoice_closure.rb
+++ app/models/input_invoice_closure.rb
@@ -12,9 +12,50 @@
 #
 
 class InputInvoiceClosure < ActiveRecord::Base
+  before_validation :set_closure_date, on: :create
+  before_validation :set_closure_amount, on: :create
+
+
   belongs_to :daily_statement
   belongs_to :input_invoice
 
   validates :closure_date, presence: true
   validates :closure_amount, presence: true
+
+  # custom validations here pls
+  validate :can_close_invoice, on: :create
+ 
+  def can_close_invoice
+    unless (input_invoice.payment_amount == daily_statement.transfer_amount) &&
+           ((input_invoice.remaining_amount + input_invoice.payment_amount) == daily_statement.remaining_amount)
+      errors.add(:closure_amount, "Cannot create Input Invoice Closure,
+                                   amounts from Daily Statement and
+                                   Input Invoice are not the same!")
+    end
+  end
+
+  private
+
+  # trying to set circulation date
+  #   to be the same as closure date
+  def set_closure_date
+    self.transaction do
+      self.closure_date = Date.today
+      input_invoice.update(circulation_date: Date.today)
+    end
+  end
+
+
+  def set_closure_amount
+    self.transaction do
+      self.closure_amount = input_invoice.payment_amount
+      current_remaining_amount = input_invoice.remaining_amount - input_invoice.payment_amount
+      # should the same be done with daily_statement?
+      input_invoice.update(remaining_amount: current_remaining_amount)
+      # how and when do i set other statuses?
+      #   we will probably set status wrong
+      #   or somethind ig closing fail?
+      daily_statement.update(status: :executed)
+    end
+  end
 end

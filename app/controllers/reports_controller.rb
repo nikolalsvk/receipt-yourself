fb5286cf68ba2337e2850408c5b4dea9eaf52213
besermenji <besermenji.vladimir@gmail.com>
2016-06-29 22:35:22 +0200

    Generic methods for sending json to sinatra app added

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -12,4 +12,21 @@ class ReportsController < ApplicationController
   def ios
   end
 
+  private
+
+  def sendJSON(email, info, path)
+    set_JSON_destination
+    RestClient.post @rg_address + path, {:data=> {email: email, 
+                                                  info: info}.to_json},
+                                        {:content_type =>:json,:accept=> :json}
+  end
+
+  def set_JSON_destination
+    @rg_address = if Rails.env.development?
+                    "http://localhost:9292"
+                  elsif Rails.env.production?
+                    "https://ry-report-generator.herokuapp.com"
+                  end
+  end
+
 end

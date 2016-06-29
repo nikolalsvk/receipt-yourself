b6749543323a01d449510dc3edbe142d9398d86b
besermenji <besermenji.vladimir@gmail.com>
2016-06-30 00:25:58 +0200

    Kif and kuf jsons generation

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,16 @@
 class ReportsController < ApplicationController
 
   def kif
+    # temp user
+    u = User.first
+    info = JSON.parse(OutputInvoice.where(company: u.company).to_json)
+    sendJSON(u.email, info, '/kif')
   end
 
   def kuf
+    u = User.first
+    info = JSON.parse(InputInvoice.where(company: u.company).first.to_json)
+    sendJSON(u.email, info, '/kuf')
   end
 
   def partner_card
@@ -17,8 +24,7 @@ class ReportsController < ApplicationController
   def sendJSON(email, info, path)
     set_JSON_destination
     RestClient.post @rg_address + path, {:data=> {email: email, 
-                                                  info: info}.to_json},
-                                        {:content_type =>:json,:accept=> :json}
+                                                  info: info}.to_json}
   end
 
   def set_JSON_destination

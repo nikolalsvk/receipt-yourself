aecd780a66e5d475ba04e853124ed4b669286b6f
besermenji <besermenji.vladimir@gmail.com>
2016-07-05 20:29:50 +0200

    Reports controller now take params from frontent

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,32 +5,43 @@ class ReportsController < ApplicationController
   def create
     parsed_reports = ActiveSupport::JSON.decode(params[:reports].to_json)
     user_id = parsed_reports['user']['id']
+    @user = User.find_by_id(user_id)
     report_type = parsed_reports['type']
     bp_contact_card_id = parsed_reports['bp_contact_card_id']
+
+    case report_type # a_variable is the variable we want to compare
+      when "kif"    #compare to 1
+        kif
+      when "kuf"    #compare to 2
+        kuf
+      when "ios"
+        ios
+      when "partner_card"
+        partner_card
+      else
+        puts "it was something else"
+    end
+
     respond_with(user_id)
   end
 
   def kif
-    # temp user
-    u = User.first
-    info = JSON.parse(OutputInvoice.where(company: u.company).to_json)
-    sendJSON(u.email, u.company, info, '/kif')
+    info = JSON.parse(OutputInvoice.where(company: @user.company).to_json)
+    sendJSON(@user.email, @user.company, info, '/kif')
   end
 
   def kuf
-    u = User.first
     input_invoices = []
-    u.company.financial_years.each do |year|
+    @user.company.financial_years.each do |year|
       year.input_invoices.each do |ii|
         input_invoices << ii
       end
     end
     info = JSON.parse(input_invoices.to_json)
-    sendJSON(u.email, u.company, info, '/kuf')
+    sendJSON(@user.email, @user.company, info, '/kuf')
   end
 
   def partner_card
-    u = User.first
     input_invoices = []
     output_invoices = []
     BusinessPartner.find_each do |partner|
@@ -39,14 +50,13 @@ class ReportsController < ApplicationController
     end
     ii = JSON.parse(input_invoices.to_json)
     oo = JSON.parse(output_invoices.to_json)
-    sendJSON(u.email, u.company, [ii, oo], '/partner_card')
+    sendJSON(@user.email, @user.company, [ii, oo], '/partner_card')
   end
 
   def ios
-    u = User.first
     opened_output = JSON.parse(OutputInvoice.opened.to_json)
     opened_input = JSON.parse(InputInvoice.opened.to_json)
-    sendJSON(u.email, u.company, [opened_output, opened_input], '/ios')
+    sendJSON(@user.email, @user.company, [opened_output, opened_input], '/ios')
   end
 
   private
